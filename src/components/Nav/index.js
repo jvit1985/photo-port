6a87ecd4a3b59024bd534f69a05df7a29a40de0c
Justin Vititoe <jvit1985@yahoo.com>
2022-07-09 20:29:02 -0500

    added ability to render only portions of page based on nav click

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,6 +7,8 @@ const {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected
 } = props;
 
     return (
@@ -19,20 +21,21 @@ const {
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a data-testId="about" href="#about">
+                        <a data-testId="about" href="#about" onClick={() => setContactSelected(false)}>
                             About Me
                         </a>
                     </li>
-                    <li className="mx-2">
-                        <span>Contact</span>
+                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                        <span onClick={() => setContactSelected(true)}>Contact</span>
                     </li>
                     {categories.map((category) => (
                         <li className={`mx-1 ${
-                            currentCategory.name === category.name && 'navActive'
+                            currentCategory.name === category.name && !contactSelected && 'navActive'
                         }`} key={category.name}>
                                 <span 
                                     onClick={() => {
-                                        setCurrentCategory(category)
+                                        setCurrentCategory(category);
+                                        setContactSelected(false);
                                     }}
                                 >
                                     {capitalizeFirstLetter(category.name)}
